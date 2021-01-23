2ddc3dece92ab6cae7d8da477973f42bfd7ecb03
Filipp Prigonskiy <jerk1991@gmail.com>
2021-01-23 13:30:58 +0300

    Целая куча для защиты

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,41 @@
-//
+var mobileWidth = 767;
+var tabletWidth = 768;
+var deviceWidth = document.body.clientWidth;
+
+// Menu
+
+var navBlock = document.querySelector('.main-nav');
+var navButton = document.querySelector('.page-header__nav-toggle');
+var navBurger = document.querySelector('.page-header__nav-open');
+var navClose = document.querySelector('.page-header__nav-close');
+
+navButton.classList.remove('page-header__nav-toggle--active');
+navBurger.classList.remove('page-header__nav-icon--active');
+navClose.classList.remove('page-header__nav-icon--active');
+
+if (deviceWidth <= mobileWidth) {
+  navBlock.classList.remove('main-nav--active');
+  navButton.classList.add('page-header__nav-toggle--active');
+  navBurger.classList.add('page-header__nav-icon--active');
+  navClose.classList.remove('page-header__nav-icon--active');
+
+  navButton.addEventListener('click', function() {
+    if (navBlock.classList.contains('main-nav--active')) {
+      navBlock.classList.remove('main-nav--active');
+      navBurger.classList.add('page-header__nav-icon--active');
+      navClose.classList.remove('page-header__nav-icon--active');
+    } else {
+      navBlock.classList.add('main-nav--active');
+      navBurger.classList.remove('page-header__nav-icon--active');
+      navClose.classList.add('page-header__nav-icon--active');
+    }
+  })
+}
+
 
 // Example block/slider
 
-var deviceWidth = document.body.clientWidth;
 var progress = document.querySelector('.example__progress');
-var tabletWidth = 768;
 
 if (progress) {
   var buttonBefore = progress.querySelector('.example__progress-button--before');
